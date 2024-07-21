9ca20f645962ad03269d7647a4ff776f76df368b
Zack Jones <justzackj@gmail.com>
2024-07-21 17:33:13 -0600

    Basic project setup

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        It works!
+      </div>
+    );
+  }
+}
